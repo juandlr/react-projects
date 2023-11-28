d75542988882ee5dba1841d7efcf2168e2047ade
juandlr <juanlr786@gmail.com>
2023-11-28 11:12:16 -0500

    react essentials, components, jsx, props & state

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,17 @@
+import CoreConcepts from "./components/CoreConcepts";
+import Header from "./components/Header/Header";
+import Examples from "./components/Examples";
+
+function App() {
+  return (
+      <div>
+        <Header/>
+        <main>
+          <CoreConcepts />
+          <Examples />
+        </main>
+      </div>
+  );
+}
+
+export default App;
